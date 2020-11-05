54f43b09ad936166dc980435d6df7d00b7bb1880
virutalsydney <70849751+virutalsydney@users.noreply.github.com>
2020-11-05 18:16:28 +0300

    .

---
+++ src/AspNetCoreDemoApp/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreDemoApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddHttpsRedirection(options => { options.HttpsPort = 443; })
+                .AddMvcCore()
+                .AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy",
+                        builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                });
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                                           ForwardedHeaders.XForwardedProto;
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseForwardedHeaders();
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO")))
+            {
+                Console.WriteLine("Use https redirection");
+                app.UseHttpsRedirection();
+            }
+
+            app
+                .UseRouting()
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseCors("CorsPolicy")
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapDefaultControllerRoute();
+                });
+        }
+    }
+}
